ad3311ed25d4e8dddfb9ad424659fae43957a961
Caleb Stucky <calebstucky@gmail.com>
2019-11-09 16:30:45 -0600

    Trying to get in a stable commit.

--- GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
+++ GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     Button submit;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
+    WebServerActivity server = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,58 +34,33 @@ public class MainActivity extends AppCompatActivity {
                 final InputStream inputStream = getResources().openRawResource(R.raw.index);
                 final InputStream inputStream2 = getResources().openRawResource(R.raw.style);
                 final InputStream inputStream3 = getResources().openRawResource(R.raw.play);
-                WebServerActivity server = null;
                 try {
                     server = new WebServerActivity(inputStream, inputStream2, inputStream3);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                statusSwitch1 = null;
                 if (simpleSwitch1.isChecked()) {
                     statusSwitch1 = simpleSwitch1.getTextOn().toString();
                     try {
 
                         server.start();
                         server.serve(null);
-                        Thread.sleep(1000000000);
                         Log.d("myTag", "Error");
                     } catch (Exception e) {
                         Log.d("myTag", "Error");
                     }
                 }
-                else
+                else if (!simpleSwitch1.isChecked()) {
                     statusSwitch1 = simpleSwitch1.getTextOff().toString();
-                   // server.end();
+                    server.stop();
+                }
 
                 Toast.makeText(getApplicationContext(), "Connection:" + statusSwitch1 + "\n", Toast.LENGTH_LONG).show();
 
             }
         });
 
-    }
-
-
-    /** Called when the usertaps the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        try {
-            final InputStream inputStream = getResources().openRawResource(R.raw.index);
-            final InputStream inputStream2 = getResources().openRawResource(R.raw.style);
-            final InputStream inputStream3 = getResources().openRawResource(R.raw.play);
-            WebServerActivity server = new WebServerActivity(inputStream, inputStream2, inputStream3);
-            server.start();
-            server.serve(null);
-            Thread.sleep(1000000000);
-            Log.d("myTag", "Error");
-        } catch(Exception e) {
-            Log.d("myTag", "Error");
-        }
-
-
-        //Intent intent = new Intent(this, WebServerActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-       // String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE.toString(), message);
-       // startActivity(intent);
     }
 
 
