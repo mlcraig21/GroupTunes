b79fafff3a77910de3e1b948006e3ab789f3d53f
Luke <lukedsharon@gmail.com>
2019-11-09 22:54:37 -0600

    basic audio features added

--- GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
+++ GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.media.MediaPlayer;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +17,7 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
     Switch simpleSwitch1;
     Button submit;
+    MediaPlayer mediaPlayer;
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     WebServerActivity server = null;
@@ -59,4 +61,36 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Connection:" + statusSwitch1 + "\n", Toast.LENGTH_LONG).show();
 
             }
-        });}}
+        });}
+    /** Called when the user taps the Play button */
+    public void songPlay(View view) {
+        // Do something in response to button
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.sound_file_1);
+        }
+        mediaPlayer.start();
+    }
+
+    /** Called when the user taps the Pause button */
+    public void songPause(View view) {
+        // Do something in response to button
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+    /** Called when the user taps the Stop button */
+    public void songStop(View view) {
+        // Do something in response to button
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+
+            Toast.makeText(MainActivity.this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
