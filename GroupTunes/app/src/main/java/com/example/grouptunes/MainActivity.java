2f84c3b539cad6bc42fd555ff2b2c6fecd4458cb
Luke <lukedsharon@gmail.com>
2019-11-10 08:40:51 -0600

    visual additions and audio continuation

--- GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
+++ GroupTunes/app/src/main/java/com/example/grouptunes/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Switch;
 import android.widget.Toast;
 import android.media.MediaPlayer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -69,7 +71,65 @@ public class MainActivity extends AppCompatActivity {
             mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.sound_file_1);
         }
         mediaPlayer.start();
+        mediaPlayer
+                .setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+
+                        String filePath2 = null;
+                        File file = null;
+                        FileInputStream inputStream = null;
+
+                        //set the filePath
+                        try {
+                            filePath2 = MainActivity.this.getFilesDir() + File.separator + file.getName();
+                            file = new File(filePath2);
+                            if (file.exists()) {
+                                inputStream = new FileInputStream(file);
+                                if (inputStream.getFD().valid()) {
+                                    System.out.println("Valid!");
+                                }
+                            }
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                            System.exit(-1);
+                        }
+
+                        //set Mediaplayer's datasource
+                        if (file.exists()) {
+                            try {
+                                mediaPlayer = new MediaPlayer();
+                                mediaPlayer.setDataSource(inputStream.getFD());
+                                inputStream.close();
+                            } catch (Exception e1) {
+                                e1.printStackTrace();
+                                System.exit(-1);
+                            }
+
+                            try {
+                                mediaPlayer.prepare();
+                            } catch (IllegalStateException e) {
+
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        //if the player is not running
+                        if (!mediaPlayer.isPlaying()) {
+                            //start the player
+                            mediaPlayer.start();
+                            Toast.makeText(MainActivity.this,
+                                    "mediaPlayer.start()", Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                });
+
+
     }
+
 
     /** Called when the user taps the Pause button */
     public void songPause(View view) {
